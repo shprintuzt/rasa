157e12fc7ea4594e51a95712b78a2d242188a0d1
Tsuyoshi Goto <shprintuzt510@gmail.com>
2020-12-01 09:28:48 +0900

    Add responses and an action to the question asking vision in x years.

--- FormFillingJa/actions/actions.py
+++ FormFillingJa/actions/actions.py
@@ -7,12 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -25,3 +24,28 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionVision(Action):
+
+    def name(self) -> Text:
+        return "action_vision"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entityList = tracker.latest_message["entities"]
+        howLong = 0 # default value
+        if len(entityList) != 0:
+            howLong = int(entityList[0]["value"])
+
+        response = "utter_vision_invalid" # default string
+        if howLong >= 2 and howLong <= 7:
+            response = "utter_vision_in_5_years"
+        elif howLong <= 15:
+            response = "utter_vision_in_10_years"
+        elif howLong <= 30:
+            response = "utter_vision_in_20_years"
+        dispatcher.utter_message(template=response)
+                     
+        return []
